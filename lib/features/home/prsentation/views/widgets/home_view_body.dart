66b124f9dad1b9924f38f6b89b286a38c4264c36
sherif-bahaa <sherifbahaa939@gmail.com>
2025-09-02 00:04:52 -0700

    add text fo hmoe & add google fonts for all app

--- lib/features/home/prsentation/views/widgets/home_view_body.dart
+++ lib/features/home/prsentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/helper.dart';
 import 'package:bookly_app/features/home/prsentation/views/widgets/custome_appbar.dart';
 import 'package:bookly_app/features/home/prsentation/views/widgets/custome_list_view.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,22 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(children: [CustomeAppbar(), CustomListView()]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomeAppbar(),
+          CustomListView(),
+          SizedBox(height: Helper.getResponsiveHeight(context, height: 40)),
+          Text(
+            "Best seller",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/consts.dart';
 import 'package:bookly_app/features/splash/persentation/views/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -13,7 +14,11 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimarycolor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kprimarycolor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
+
       debugShowCheckedModeBanner: false,
 
       home: Splash(),
