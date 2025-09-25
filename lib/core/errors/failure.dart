3a8b5ce134bf10bc4cfe023babce2316f23acab7
sherif-bahaa <sherifbahaa939@gmail.com>
2025-09-25 00:19:16 -0700

    create server failure from dio error

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,5 +1,42 @@
-abstract class Failure{
-  static ServerFailure (String toString) {
+import 'package:dio/dio.dart';
 
+abstract class Failure{
+  final String message;
+  Failure(this.message);
+}
+class ServerFailure extends Failure{
+  ServerFailure(super.message);
+  factory ServerFailure.fromDioError(DioException message){
+    switch(message.type){
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout with ApiServer');
+      case DioExceptionType.badResponse:
+        if(message.response?.statusCode==400){
+          return ServerFailure('Bad request');
+        }else if(message.response?.statusCode==401||message.response?.statusCode==403){
+          return ServerFailure('Unauthorized request');
+        }else if(message.response?.statusCode==404){
+          return ServerFailure('Not found');
+        }else if(message.response?.statusCode==500){
+          return ServerFailure('Internal server error');
+        }
+        return ServerFailure('Received invalid status code: ${message.response?.statusCode}');
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection to ApiServer failed due to internet connection');
+      case DioExceptionType.unknown:
+        if(message.message?.contains('SocketException')==true){
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected error occured');
+      default:
+        return ServerFailure('Opps there was an error, please try again later.');
+    }
+    
   }
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/utils/api_services.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiServices apiServices;
@@ -17,7 +18,11 @@ class HomeRepoImpl implements HomeRepo {
   }
   return Right(books);
 } on Exception catch (e) {
-  return Left(Failure.ServerFailure(e.toString()));
+  if(e is DioException){
+    return Left(ServerFailure.fromDioError(e ));
+  }
+  return Left(ServerFailure(e.toString()));
+  
 }
 
    
